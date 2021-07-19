07aa11b7c5c429577904924ade92b88e0307e22c
hiraoka.osamu <hiraoka.osamu@classmethod.jp>
2021-07-19 17:19:07 +0900

    UnitTestにtestBeerForRowを追加

---
+++ AwesomeBeerApp-MVPTests/Beer+BeersMock.swift
@@ -0,0 +1,30 @@
+//
+//  Beer+Mock.swift
+//  AwesomeBeerApp-MVP
+//
+//  Created by Osamu Hiraoka on 2021/07/19.
+//
+
+import Foundation
+
+extension Beer {
+    
+    static func mockBeers() -> [Beer] {
+        let beers = [
+            Beer(id: 1,
+                 name: "Asahi Super dry",
+                 tagline: "さらりとした飲み口、キレ味さえる、いわば辛口の生ビールです。",
+                 firstBrewed: "1987年",
+                 description: ""),
+            Beer(id: 2,
+                 name: "キリン一番搾り生ビール",
+                 tagline: "おいしさに妥協しない、それが一番搾り®︎製法",
+                 firstBrewed: "1990年",
+                 description: ""),
+            Beer(id: 3, name: "ヱビスビール", tagline: "旨味あふれる、ふくよかなコク。",
+                 firstBrewed: "1890年",
+                 description: "1890年の誕生以来、本場ドイツのおいしさにこだわり続け、本物のビールの先駆者として、変わらぬおいしさを届け続けてきたヱビス。麦芽100％、ヱビス酵母、ふんだんに使用されたバイエルン産アロマホップ、そして長期熟成。吟味しつくされた原料と製法がつむぐ、旨味あふれる、ふくよかなコク。ヱビスビール。それは、唯一無二のビールです。")
+        ]
+        return beers
+    }
+}
---
+++ AwesomeBeerApp-MVPTests/BeerListPresenterTests.swift
@@ -0,0 +1,91 @@
+//
+//  BeerListPresenter.swift
+//  AwesomeBeerApp-MVPTests
+//
+//  Created by Osamu Hiraoka on 2021/07/19.
+//
+
+import XCTest
+@testable import AwesomeBeerApp_MVP
+
+class BeerListPresenterInputMock: BeerListPresenterInput {
+    
+    private (set) var beers = Beer.mockBeers()
+    
+    var numberOfBeers: Int {
+        return beers.count
+    }
+    
+    func beer(forRow row: Int) -> Beer? {
+        if row >= beers.count {
+            return nil
+        }
+        return beers[row]
+    }
+    
+    func didSelectRowAt(_ indexPath: IndexPath) {
+//        guard let beer = beer(forRow: indexPath.row)
+//        else { return }
+//        outputView?.transitionToBeerDetail(of: beer)
+    }
+    
+    func loadBeers() {
+        PunkAPIManager.instance.loadBeer { [weak self] result in
+            switch result {
+            case .failure(let error):
+                print(error)
+//                self?.outputView?.didFailToLoadBeers(with: error)
+            case .success(let loadedBeer):
+                self?.beers = loadedBeer
+                guard let beers = self?.beers
+                else { return }
+                print(beers)
+//                self?.outputView?.didLoad(beers)
+            }
+        }
+    }
+}
+
+class BeerListPresenterTests: XCTestCase {
+    
+    var presenter = BeerListPresenterInputMock()
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func testBeerForRow() throws {
+        // mockBeersのカウントは3
+        XCTContext.runActivity(named: "rowの値がbeersのcountより小さい場合は戻り値がnilではないこと") { _ in
+            let beer = presenter.beer(forRow: 0)
+            XCTAssertNotNil(beer, "beerが返されること")
+        }
+        
+        XCTContext.runActivity(named: "rowの値がbeersのcount - 1と等しい場合は戻り値がnilではないこと") { _ in
+            let beer = presenter.beer(forRow: 2)
+            XCTAssertNotNil(beer, "beerが返されること")
+        }
+        
+        XCTContext.runActivity(named: "rowの値がbeersのcountと等しい場合は戻り値がnilであること") { _ in
+            let beer = presenter.beer(forRow: 3)
+            XCTAssertNil(beer)
+        }
+        
+        XCTContext.runActivity(named: "rowの値がbeersのcountより大きい場合は戻り値がnilであること") { _ in
+            let beer = presenter.beer(forRow: 4)
+            XCTAssertNil(beer)
+        }
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
