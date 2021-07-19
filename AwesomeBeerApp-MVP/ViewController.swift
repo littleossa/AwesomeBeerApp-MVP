00ac714a5038f44308acd2838531490fb99c4e43
hiraoka.osamu <hiraoka.osamu@classmethod.jp>
2021-07-19 09:00:06 +0900

    Initial Commit

---
+++ AwesomeBeerApp-MVP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AwesomeBeerApp-MVP
+//
+//  Created by Osamu Hiraoka on 2021/07/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
