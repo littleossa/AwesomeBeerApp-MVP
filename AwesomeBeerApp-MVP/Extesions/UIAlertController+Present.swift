b523d6388be281ff6993f3b037c2dd3cea2cbbf2
hiraoka.osamu <hiraoka.osamu@classmethod.jp>
2021-07-23 06:37:32 +0900

    UIAlertController+PresentのshouldWorkOnMainTheadのタイポを修正

--- AwesomeBeerApp-MVP/Extesions/UIAlertController+Present.swift
+++ AwesomeBeerApp-MVP/Extesions/UIAlertController+Present.swift
@@ -9,13 +9,13 @@ import UIKit
 
 extension UIAlertController {
     
-    class func present(at viewController: UIViewController, title: String, messsage: String, cancelActionTitle: String, shouldWorkOnMainThead: Bool = false) {
+    class func present(at viewController: UIViewController, title: String, messsage: String, cancelActionTitle: String, shouldWorkOnMainThread: Bool = false) {
         
         let alert = UIAlertController(title: title, message: messsage, preferredStyle: .alert)
         let action = UIAlertAction(title: cancelActionTitle, style: .cancel)
         alert.addAction(action)
         
-        if shouldWorkOnMainThead {
+        if shouldWorkOnMainThread {
             DispatchQueue.main.async {
                 viewController.present(alert, animated: true)
             }
