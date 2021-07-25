03f328dadc506ce5793b488da39e55c194f94cac
hiraoka.osamu <hiraoka.osamu@classmethod.jp>
2021-07-25 11:22:35 +0900

    BeerDetail.stroyboardとBeerDetailViewControllerの作成

---
+++ AwesomeBeerApp-MVP/View/BeerDetail/BeerDetailViewController.swift
@@ -0,0 +1,31 @@
+//
+//  BeerDetailViewController.swift
+//  AwesomeBeerApp-MVP
+//
+//  Created by Osamu Hiraoka on 2021/07/25.
+//
+
+import UIKit
+
+class BeerDetailViewController: UIViewController {
+
+    @IBOutlet private weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
